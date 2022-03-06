771fdf6bbf61eb2db68258b351bc2d1d8f8158bc
Stefan Zemljic <stefan.zemljic@protonmail.com>
2022-03-06 15:37:07 +0100

    Installed chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Box, Image} from '@chakra-ui/react';
 import { FaBeer } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Box w={500}>
+          <Image src={logo} className="App-logo" alt="logo" />
+        </Box>
         <FaBeer color="#3592FF" size={100}/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
