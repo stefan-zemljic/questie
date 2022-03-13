28f07438a139cc90a700dae1aff9361e9fddb515
Stefan Zemljic <stefan.zemljic@protonmail.com>
2022-03-13 08:15:43 +0100

    Implemented press enter to submit

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -18,7 +18,7 @@ function centerCard(element: JSX.Element) {
     </Center>
 }
 
-function Login(props: {supabaseClient: SupabaseClient, onLoggedIn: (user: User, session: Session) => void}) {
+function Login(props: { supabaseClient: SupabaseClient, onLoggedIn: (user: User, session: Session) => void }) {
     const supabase = props.supabaseClient;
     const [state, setState] = useState(0);
     const [email, setEmail] = useState("");
@@ -34,31 +34,79 @@ function Login(props: {supabaseClient: SupabaseClient, onLoggedIn: (user: User, 
     ));
     const {'access_token': accessToken} = hash;
 
+    async function savePassword() {
+        if (password !== password2) {
+            setError('Password mismatch')
+            return
+        }
+        const {error} = await supabase.auth.api.updateUser(accessToken, {password})
+        if (error) {
+            setError(JSON.stringify(error))
+        } else {
+            setEmail('');
+            setPassword2('');
+            setPassword('');
+            setHash('');
+            setError(undefined);
+            setState(0);
+        }
+    }
+
+    async function sendResetLink() {
+        const {error} = await supabase.auth.api.resetPasswordForEmail(email);
+        if (error) {
+            setError(JSON.stringify(error))
+        } else {
+            setError(undefined);
+            setState(3);
+        }
+    }
+
+    async function login() {
+        const {user, session, error} = await supabase.auth.signIn({
+            email: email,
+            password: password
+        })
+        if (error) {
+            setError(JSON.stringify(error))
+        } else if (user && session) {
+            setError(undefined);
+            props.onLoggedIn(user, session);
+        } else {
+            setError("Internal Error 127")
+        }
+    }
+
+    async function register() {
+        if (password !== password2) {
+            setError('Password mismatch');
+            return
+        }
+        const {error} = await supabase.auth.signUp({
+            email: email,
+            password: password
+        });
+        if (error) {
+            setError(JSON.stringify(error))
+        } else {
+            setError(undefined);
+            setState(1);
+        }
+    }
+
     if (accessToken) {
         return centerCard(<Flex flexDir="column" gap="8px">
             <Box height="4px"/>
             <Input placeholder="new password" type="password" value={password}
-                   onChange={(e) => setPassword(e.target.value)}/>
+                   onChange={e => setPassword(e.target.value)}
+                   onKeyPress={e => {
+                       if (e.key === 'Enter') savePassword().then();
+                   }}
+            />
             <Input placeholder="retype password" type="password" value={password2}
-                   onChange={(e) => setPassword2(e.target.value)}/>
+                   onChange={e => setPassword2(e.target.value)}/>
             <Box textColor="#FF3377">{error ?? ''}</Box>
-            <Button colorScheme="blue" onClick={async () => {
-                if (password !== password2) {
-                    setError('Password mismatch')
-                    return
-                }
-                const {error} = await supabase.auth.api.updateUser(accessToken, {password})
-                if (error) {
-                    setError(JSON.stringify(error))
-                } else {
-                    setEmail('');
-                    setPassword2('');
-                    setPassword('');
-                    setHash('');
-                    setError(undefined);
-                    setState(0);
-                }
-            }}>Save Password</Button>
+            <Button colorScheme="blue" onClick={savePassword}>Save Password</Button>
         </Flex>);
     }
 
@@ -70,17 +118,13 @@ function Login(props: {supabaseClient: SupabaseClient, onLoggedIn: (user: User, 
     if (state === 2) {
         return centerCard(<Flex flexDir="column" gap="8px">
             <Input placeholder="email" type="email" value={email}
-                   onChange={(e) => setEmail(e.target.value)}/>
+                   onChange={e => setEmail(e.target.value)}
+                   onKeyPress={e => {
+                       if (e.key === 'Enter') sendResetLink().then();
+                   }}
+            />
             <Box textColor="#FF3377">{error ?? ''}</Box>
-            <Button colorScheme="blue" onClick={async () => {
-                const {error} = await supabase.auth.api.resetPasswordForEmail(email);
-                if (error) {
-                    setError(JSON.stringify(error))
-                } else {
-                    setError(undefined);
-                    setState(3);
-                }
-            }}>Send reset Link</Button>
+            <Button colorScheme="blue" onClick={sendResetLink}>Send reset Link</Button>
         </Flex>);
     }
 
@@ -108,50 +152,28 @@ function Login(props: {supabaseClient: SupabaseClient, onLoggedIn: (user: User, 
                     <Box height="4px"/>
                     <Input placeholder="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <Input placeholder="password" type="password" value={password}
-                           onChange={(e) => setPassword(e.target.value)}/>
+                           onChange={(e) => setPassword(e.target.value)}
+                           onKeyPress={e => {
+                               if (e.key === 'Enter') login().then();
+                           }}/>
                     <Link color="#4477ff" onClick={() => setState(2)}>forgot password?</Link>
                     <Box textColor="#FF3377">{error ?? ''}</Box>
-                    <Button colorScheme="blue" onClick={async () => {
-                        const {user, session, error} = await supabase.auth.signIn({
-                            email: email,
-                            password: password
-                        })
-                        if (error) {
-                            setError(JSON.stringify(error))
-                        } else if (user && session) {
-                            setError(undefined);
-                            props.onLoggedIn(user, session);
-                        } else {
-                            setError("Internal Error 127")
-                        }
-                    }}>Login</Button>
+                    <Button colorScheme="blue" onClick={login}>Login</Button>
                 </Flex>
             </TabPanel>
             <TabPanel>
                 <Flex flexDir="column" gap="8px">
                     <Box height="4px"/>
-                    <Input placeholder="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <Input placeholder="email" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
                     <Input placeholder="password" type="password" value={password}
-                           onChange={(e) => setPassword(e.target.value)}/>
+                           onChange={e => setPassword(e.target.value)}/>
                     <Input placeholder="retype password" type="password" value={password2}
-                           onChange={(e) => setPassword2(e.target.value)}/>
+                           onChange={e => setPassword2(e.target.value)}
+                           onKeyPress={e => {
+                               if (e.key === 'Enter') register().then();
+                           }}/>
                     <Box textColor="#FF3377">{error ?? ''}</Box>
-                    <Button colorScheme="blue" onClick={async () => {
-                        if (password !== password2) {
-                            setError('Password mismatch');
-                            return
-                        }
-                        const {error} = await supabase.auth.signUp({
-                            email: email,
-                            password: password
-                        });
-                        if (error) {
-                            setError(JSON.stringify(error))
-                        } else {
-                            setError(undefined);
-                            setState(1);
-                        }
-                    }}>Register</Button>
+                    <Button colorScheme="blue" onClick={register}>Register</Button>
                 </Flex>
             </TabPanel>
         </TabPanels>
